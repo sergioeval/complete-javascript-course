c768826f91de7a2b4fee6362f02874e067ae8625
SERGIO ESTRADA VALENZUELA <sergioev@mx1.ibm.com>
2021-02-12 18:17:22 -0600

    updates

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,38 @@
+let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is fun');
+
+console.log(40+8+23-10)
+
+//values - smalles unit of information
+console.log('jonas')
+console.log(23)
+
+//variables
+
+let firstName = 'Bob';
+console.log(firstName)
+
+// conventions and rules for namming variables
+// more than 1 words firstName 
+// or first_name this is more popular in ther languages
+// we can not use number in the vairable at the start
+
+//new is areserved keyword in js
+// another is reserved is function
+//name is a reserved keyword 
+
+// do not start a variable with uppercase 
+
+// contats are usually write in uppercase like 
+let PI = 3.1415;
+
+// variables should be self descriptive 
+// example is
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher'
+
+//bad example is 
+let job1 = 'Programmer'
+let job2 = 'Teacher'
+
+
